ba58bfdf7d566509d40973aa7592670967332cab
cristianv <c.velandiag@uniandes.edu.co>
2022-12-09 01:28:09 -0500

    mi primer commit

--- trueque/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TruequeConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'trueque'
--- trueque/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-# from django.conf.urls import url
-from .views import Health
-
-
-urlpatterns = [
-    path('', Health.as_view(), name='health'),
-]
